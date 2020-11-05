5f8f1c245617c65b7c2263ee1022b15170291d51
Danilo Trindade <danilotrindade87@gmail.com>
2020-11-05 10:18:50 -0300

    Número secreto aleatório e níveis de dificuldade implementados
    Agora o jogador poderá escolher um nível de dificuldade e ter mais ou menos tentativas para acertar o número secreto. O número secreto será sorteado entre 1 e 100 de forma aleatória.

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,13 +1,32 @@
-print("**************************************")
-print("** Bem-vindo ao Jogo de Adivinhação **")
-print("**************************************")
+import random
 
-numero_secreto = 10
-total_tentativas = 3
+print("****************************************")
+print("*** Bem-vindo ao Jogo de Adivinhação ***")
+print("****************************************")
+
+numero_secreto = random.randrange(1,101)
+total_tentativas = 0
+
+# Apresentando níveis de dificuldde
+print("* Qual nível de dificuldade você quer? *")
+print("* (1) Fácil | (2) Médio | (3) Difícil  *")
+print("****************************************")
+
+# Capturando escolha do jogador via teclado
+nivel = int(input("Digite o nível: "))
+
+# Setando a variável 'total_tentativas' a partir do nível escolhido pelo
+# jogador onde 1 = 20 tentativas, 2 = 10 tetaticas e 3 = 5 tentativas.
+if (nivel == 1):
+    total_tentativas = 20
+elif (nivel == 2):
+    total_tentativas = 10
+else:
+    total_tentativas = 5
 
 # Limitando a quantidade de jogadas
 for rodada in range(1, total_tentativas + 1):
-    print("Tentativa: {} de {}".format(rodada, total_tentativas)) # interpolação de string
+    print("Tentativa: {} de {}".format(rodada, total_tentativas), numero_secreto) # interpolação de string
     numero_digitado = int(input("Qual é o número secreto entre 1 e 100: ")) # convertendo entrada para int
 
     if (numero_digitado < 1 or numero_digitado > 100):
