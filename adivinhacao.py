d5dbcdc77d9008f1b4e7a525d14969c8e8e938ae
Danilo Trindade <danilotrindade87@gmail.com>
2020-11-05 08:54:34 -0300

    Lógica básica do jogo de adivinhação implementado

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,18 @@
-print("Bem-vindo ao Jogo de Adivinhação")
+print("**************************************")
+print("** Bem-vindo ao Jogo de Adivinhação **")
+print("**************************************")
+
+numero_secreto = 10
+numero_digitado = int(input("Adivinhe qual é o número secreto: ")) # convertendo entrada para int
+
+acertou = numero_digitado == numero_secreto
+maior   = numero_digitado >  numero_secreto
+menor   = numero_digitado <  numero_secreto
+
+if (acertou):
+    print("Você acertou! Parabéns!")
+else:
+    if (maior):
+        print("Você errou! Tente um número MENOR.")
+    elif (menor):
+        print("Você errou! Tente um número MAIOR.")
