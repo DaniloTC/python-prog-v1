f44af15259243ce4459f820659eb8f9922930d29
Danilo Trindade <danilotrindade87@gmail.com>
2020-11-06 04:48:37 -0300

    Arquivo principal transformado em módulo

--- principal.py
+++ principal.py
@@ -2,26 +2,30 @@ import adivinhacao
 import imc
 import matematica
 
-# Apresentando opções para o usuário
-print("***************************")
-print("***  Escolha uma opção  ***")
-print("***************************")
-print("* (1) Jogo de Adivinhação *")
-print("* (2) Matemática          *")
-print("* (3) Calcular IMC        *")
-print("***************************")
+def inicial():
+    # Apresentando opções para o usuário
+    print("***************************")
+    print("***  Escolha uma opção  ***")
+    print("***************************")
+    print("* (1) Jogo de Adivinhação *")
+    print("* (2) Matemática          *")
+    print("* (3) Calcular IMC        *")
+    print("***************************")
 
-# Capturando escolha do usuário
-escolha = int(input("O que deseja fazer? "))
+    # Capturando escolha do usuário
+    escolha = int(input("O que deseja fazer? "))
 
-# Processando a escolha do usuário e executando o programa
-# que foi escolhido a partir do menu.
-if (escolha == 1):
-    adivinhacao.jogar_adivinhacao()
-elif (escolha == 2):
-    matematica.operacoes()
-elif (escolha == 3):
-    imc.calcular_imc()
-else:
-    print("Calcular imc")
+    # Processando a escolha do usuário e executando o programa
+    # que foi escolhido a partir do menu.
+    if (escolha == 1):
+        adivinhacao.jogar_adivinhacao()
+    elif (escolha == 2):
+        matematica.operacoes()
+    elif (escolha == 3):
+        imc.calcular_imc()
+    else:
+        print("Calcular imc")
 
+# Modularizando este arquivo
+if(__name__ == "__main__"):
+    inicial()
