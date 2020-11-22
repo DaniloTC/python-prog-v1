f8080db009cd12617fe211dbbdbeadc9e8935877
Danilo Castro <danilotrindade87@gmail.com>
2020-11-22 20:17:22 -0300

    Primeira lógica do jogo implementada
    Esta lógica faz a leitura de duas variáveis para determinar se o jogo continua ou não com uma estrutura de loop while.  Em seguida, pede ao usuário uma letra e com outra estrutura de loop iteramos sobre todas as letras da palavra secreta. Por fim, comparamos o chute do usuário com a letra encontrada na palavra, imprimindo a letra ou incrementando a variável que indica o índice de cada letra na palavra.
    
    O problema: while não tem critérios para encerrar o jogo, além de não encontrar uma letra se for digitado em maiúsculo, mesmo se existir na palavra. Precisamos resolver isso.

--- forca.py
+++ forca.py
@@ -3,7 +3,21 @@ def jogar_forca():
     print("Bem-vindo(a) ao jogo da Forca")
     print("*****************************")
 
-    print("Jogando forca ...")
+    palavra_secreta = "banana" # por enquanto a palavra secreta permenecerá fixa
+
+    enforcou  = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+
+        chute = input("Escolha uma letra? ")
+        index = 0
+
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            else:
+                index += 1
 
 if (__name__ == "__main__"):
     jogar_forca()
