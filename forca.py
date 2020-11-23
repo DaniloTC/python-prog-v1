8a1c0a4b81b9c9fa701a2e5c3f4a3ad17c18ecd9
Danilo Castro <danilotrindade87@gmail.com>
2020-11-22 21:04:36 -0300

    Lógica para sair do while implementada
    Também implementei a lógica que mostrará a letra acertada na posição em que ela existe na palavra secreta, por exemplo, na palavra PYTHON escolhemos a letra P, T, N que será apresentado no formato P _ T _ _ N cada uma na sua posição até completar a palavra.

--- forca.py
+++ forca.py
@@ -3,20 +3,31 @@ def jogar_forca():
     print("Bem-vindo(a) ao jogo da Forca")
     print("*****************************")
 
-    palavra_secreta = "banana" # por enquanto a palavra secreta permenecerá fixa
+    palavra_secreta = "banana".upper() # por enquanto a palavra secreta permenecerá fixa
+    letras_acertadas = ["_","_","_","_","_","_"] # permanecerá fixa por enquanto
 
-    enforcou  = False
+    enforcou = False
     acertou = False
+
+    print(letras_acertadas) # mostra quantidade de letras na palavra
 
     while (not enforcou and not acertou):
 
-        chute = input("Escolha uma letra? ")
+        chute = input("Escolha uma letra? ").strip().upper() # limpa espaços e transforma em maiúscula
         index = 0
 
         for letra in palavra_secreta:
             if (chute == letra):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
+                letras_acertadas[index] = letra
             index += 1
+        
+        print(letras_acertadas) # coloca letra acertada na posição certa dentro da palavra
+
+        # lógica para sair do while.
+        # redefinir variável 'enforcou' caso não exista o caractere ( _ ) na lista.
+        acertou = "_" not in letras_acertadas
+
+    print("Fim de jogo!")
 
 if (__name__ == "__main__"):
     jogar_forca()
